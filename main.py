834de82aebf4ecf97115af11cff143606a84adce
HaZeRt <viktoryura2003@gmail.com>
2024-04-28 18:33:20 +0500

    new

--- main.py
+++ main.py
@@ -49,13 +49,12 @@ def handle_post():
                                     if isinstance(employee, dict) and 'user_id' in employee:
                                         if str(employee['user_id']) == str(courier_id):
                                             login = employee.get('login')
-                                            print(f"courier_id: {courier_id}, login: {login}")
-
                                             username = login.split('@')[0]
 
                                             if 'products' in item:
                                                 for product in item['products']:
                                                     product_id = product['product_id']
+                                                    product_num = int(float(product['num']))
 
                                                     # Формирование URL для запроса на получение информации о продукте
                                                     product_url = f"https://joinposter.com/api/menu.getProduct?token={TOKEN}&product_id={product_id}"
@@ -66,82 +65,91 @@ def handle_post():
                                                     # Проверка успешности запроса
                                                     if product_response.status_code == 200:
                                                         product_data = product_response.json()
-                                                        print(product_data)
                                                         # Проверка наличия данных и наличия ключа 'product_name'
                                                         if 'response' in product_data and 'product_name' in \
                                                                 product_data['response']:
                                                             product_name = product_data['response']['product_name']
-                                                            print(f"Название продукта: {product_name}")
 
                                                             # Добавляем название продукта в общий список
-                                                            product_names_str.append(product_name)
+                                                            product_names_str.append(f"{product_name} - {product_num}")
 
-                                                    print("Список всех продуктов:", product_names_str)
-
-
+                                                    # print("Список всех продуктов:", product_names_str)
 
             # Теперь вы можете включить product_name в ваше сообщение
-                                                    message = f"""
-                                                =================x
-Заказ № {data["response"][0].get('transaction_id', '')}
-
-Заведение:
-{data_request.get('account', 'нет данных')}
-
-Доставить до:
-{data["response"][0]['delivery'].get('delivery_time', '')}
-
-Клиент:
-{data["response"][0].get('client_firstname', '')} {data["response"][0].get('client_lastname', '')}
-
-Телефон:
-{data['response'][0].get('client_phone', '')}
 
 
-Адрес: 
-{data['response'][0]['delivery'].get('address1', '')}
-{data['response'][0]['delivery'].get('address2', '')}
+                                            product_text = '\n'.join(product_names_str)
+                                            summa = data['response'][0].get('sum', '')
 
-
-Координаты:
-{data["response"][0]['delivery'].get('lat', '')},{data["response"][0]['delivery'].get('lng', '')}
-
-
-Комментарий курьеру:
-{data['response'][0]['delivery'].get('comment', '')}
-
-
-Комментарий к чеку:
-{data["response"][0].get("transaction_comment", '')}
-
-
-Товары:
-Заказ содержит следующие продукты: {product_names_str}.
-
-
-К оплате: {data["response"][0]['sum']} €
-Купюры клиента: {data["response"][0]['payed_cash']} €
-Баланс бонусов: {data["response"][0]['payed_bonus']}
-Метод оплаты: {'Наличные' if data["response"][0]['pay_type'] == '0' else 'Карта' if data["response"][0]['pay_type'] == '1' else 'Другой'}
-=================
-                                            """
-
-                                                    data = {
-                                                        "chat_id": int(username),
-                                                        "message": message,
-                                                        "transaction_id": {data["response"][0].get('transaction_id', '')},
-                                                        "spot_id": {data["response"][0].get('spot_id', '')}                                                    }
-
-                                                    try:
-                                                        socketio.emit('message', data)
-                                                    except json.JSONDecodeError as e:
-                                                        print("Error decoding JSON:", e)
-
-                                                    break
-                                                else:
-                                                    print("Не удалось получить название продукта.")
+                                            # Если длина числа больше или равна 2, добавляем точку и форматируем как дробное число
+                                            if len(summa) >= 2:
+                                                summa = f"{summa[:-2]}.{summa[-2:]}"
+                                            # Если длина числа меньше 2, добавляем нуль в конце, чтобы иметь два знака после точки
                                             else:
-                                                print("Ошибка при запросе информации о продукте.")
+                                                summa = f"{summa}.{'0'.zfill(2)}"
+
+
+                                            message = f"""
+                                                                                                =================x
+                                                Заказ № {data["response"][0].get('transaction_id', '')}
+
+                                                Заведение:
+                                                {data_request.get('account', 'нет данных')}
+
+                                                Доставить до:
+                                                {data["response"][0]['delivery'].get('delivery_time', '')}
+
+                                                Клиент:
+                                                {data["response"][0].get('client_firstname', '')} {data["response"][0].get('client_lastname', '')}
+
+                                                Телефон:
+                                                {data['response'][0].get('client_phone', '')}
+
+
+                                                Адрес:
+                                                {data['response'][0]['delivery'].get('address1', '')}
+                                                {data['response'][0]['delivery'].get('address2', '')}
+
+
+                                                Координаты:
+                                                {data["response"][0]['delivery'].get('lat', '')},{data["response"][0]['delivery'].get('lng', '')}
+
+
+                                                Комментарий курьеру:
+                                                {data['response'][0]['delivery'].get('comment', '')}
+
+
+                                                Комментарий к чеку:
+                                                {data["response"][0].get("transaction_comment", '')}
+
+
+                                                Товары:
+                                                Заказ содержит следующие продукты: 
+                                                {product_text}.
+
+
+                                                К оплате: {summa} 
+                                                
+                                                Метод оплаты: {'Наличные' if data["response"][0]['pay_type'] == '0' else 'Карта' if data["response"][0]['pay_type'] == '1' else 'Другой'}
+                                                =================
+                                                                                            """
+
+                                            data = {
+                                                    "chat_id": int(username),
+                                                    "message": message,
+                                                    "transaction_id": data["response"][0].get('transaction_id', ''),
+                                                    "spot_id": data["response"][0].get('spot_id', ''),
+                                                    "spot_tablet_id" : data['response'][0]['history'][0]['spot_tablet_id'],
+                                                    "payed_cash":summa,
+                                                    "address": f"{data['response'][0]['delivery'].get('address1', '')} {data['response'][0]['delivery'].get('address2', '')}"
+                                            }
+
+
+                                            try:
+                                                socketio.emit('message', data)
+                                            except json.JSONDecodeError as e:
+                                                print("Error decoding JSON:", e)
+                                            break
                                     else:
                                         print("Некорректные данные о пользователе:", employee)
                         else:
@@ -155,19 +163,9 @@ def handle_connect():
     emit('connected', {'status': 'success', 'message': 'You are now connected to the server'})
     return jsonify(message="Hello, World!"), 200
 
-
-# Обрабатывать message
-@socketio.on('message')
-def handle_message(message):
-    try:
-        data = json.loads(message)
-        emit('message', data, broadcast=True)
-    except json.JSONDecodeError as e:
-        print("Error decoding JSON:", e)
-
-
 if __name__ == '__main__':
     socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
 
-print("123")
+
+
 # Витя пидор
